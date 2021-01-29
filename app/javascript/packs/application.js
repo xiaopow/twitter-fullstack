74b259c06e80a5cea71dae7d0096dcaec646a4e7
xiaopow <xiaoyang88@gmail.com>
2021-01-29 14:37:16 +0800

    some small changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,5 @@
 // const imagePath = (name) => images(name, true)
 import 'jquery/src/jquery'
 import 'popper.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
